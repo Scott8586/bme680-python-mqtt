a6b40313ff5261cd94d08218002ed011f62bf209
Scott Presnell <srp@tworoads.net>
2020-06-18 08:05:02 -0700

    remove .0 from round - so it reports integer result

--- bme680-mqtt-daemon.py
+++ bme680-mqtt-daemon.py
@@ -110,7 +110,7 @@ def publish_mqtt(client, sensor_data, options, file_handle, air_quality_score=0,
             
     else:
         data = {}
-        data['gas'] = round(gas, 0)
+        data['gas'] = round(gas)
         data['humidity'] = round(hum, 1)
         data['temperature'] = round(temp_F, 1)
         data['pressure'] = round(press_A, 2)
@@ -120,7 +120,7 @@ def publish_mqtt(client, sensor_data, options, file_handle, air_quality_score=0,
             data['air_quality'] = round(air_quality_score, 2)
         data['burn_in'] = str(gas_baseline !=0)
         if gas_baseline != 0:
-            data['gas_baseline'] = round(gas_baseline, 0) 
+            data['gas_baseline'] = round(gas_baseline) 
         data['timestamp'] = curr_datetime.replace(microsecond=0).isoformat()
 
         json_data = json.dumps(data)
